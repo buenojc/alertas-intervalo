189dafb237224cd0d5aa68c971b93d8ef32ac13d
Julio Cesar Bueno <buenojc@outlook.com>
2022-10-20 23:39:59 -0300

    Adiciona seletor de quantidade de repetição

--- scripts.js
+++ scripts.js
@@ -1,5 +1,33 @@
 const setTime = document.getElementById('set-time');
 const setRepetition = document.getElementById('set-repetition')
+const buttonAddTime = document.getElementById('add-time');
+const buttonAddRepetition = document.getElementById('add-repetition')
+const buttonRemoveRepetition = document.getElementById('remove-repetition')
 
 setTime.value = '00:00'
-setRepetition.value = 0
+setRepetition.value = 1
+
+buttonAddRepetition.addEventListener('click', (e) => {
+    e.preventDefault();
+    setRepetition.value = parseInt(setRepetition.value) + 1;
+
+})
+
+
+buttonRemoveRepetition.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(setRepetition.value > 1){
+        setRepetition.value = parseInt(setRepetition.value) - 1;
+    }
+})
+
+
+setRepetition.addEventListener('keydown', e => {
+    const regex = /^[0-9]*$/;
+
+    if(!regex.test(e.key) && e.key !== 'Backspace'){
+        e.preventDefault()
+    }
+})
+
+
